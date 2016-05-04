5283aead3ca9efb5c690942f45cfec2550b0b358
刘瑶 <1094131083@qq.com>
2016-05-04 20:40:26 +0800

    9.1

--- src/client/ChatServer.java
+++ src/client/ChatServer.java
@@ -6,24 +6,45 @@ public class ChatServer {
 
 	public static void main(String[] args) {
 		boolean started=false;
+		ServerSocket ss=null;
+		Socket s =null;
+		DataInputStream dis =null;
 		try {
-			ServerSocket ss = new ServerSocket(8888);//此类实现服务器套接字；
+			 ss = new ServerSocket(8888);//此类实现服务器套接字；
+		}
+		
+			catch(IOException e){
+                e.printStackTrace(); 				
+			}
+		try{
 			started=true;//服务器启动；
 			while(started) {
 				boolean bConnected=false;
-				Socket s = ss.accept();//接收
+				 s = ss.accept();//接收
 				bConnected=true;//接收了客户端的连接；
 System.out.println("a client connected!");
-				DataInputStream dis = new DataInputStream(s.getInputStream());
+			    dis = new DataInputStream(s.getInputStream());
 				while(bConnected){
-				String str = dis.readUTF();
+				String str = dis.readUTF();//阻塞式
 				System.out.println(str);
 				}
 				dis.close();
 			}
+		
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("Client closed !");
+		//	e.printStackTrace();
+		}
+	finally{
+		try{
+			s.close();
+			dis.close();
+		}
+		catch(IOException e1){
+			e1.printStackTrace();
+			
 		}
 	}
 
+}
 }
